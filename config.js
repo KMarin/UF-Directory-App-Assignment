bf0bba4471e9b006b9cf7db6ea043f57f1ba8a03
Kevin Marin <kmarin@ufl.edu>
2016-01-26 21:18:36 -0500

    Chenges

---
+++ config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Kevin on 1/19/2016.
+ */
+
+module.exports = {
+    db: {
+        uri: 'mongodb://kmarin:Tuxy3530@ds047095.mongolab.com:47095/10b-directory-app', //place the URI of your mongo database here.
+    }
+};
